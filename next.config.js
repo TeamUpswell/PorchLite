f1aa37810242b7c67cea5ec5f7621f4a50b9477d
Drew Bernard <82413471+TeamUpswell@users.noreply.github.com>
2025-06-02 22:01:26 -0700

    Add Supabase domain to Next.js image configuration

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Add this images configuration
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
+  },
+
   eslint: {
     ignoreDuringBuilds: true,
   },
